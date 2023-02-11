cfaa950adcb3aa0b929a5b2b4fbc8839bb973df1
Yurysi <92383030+Yurysi@users.noreply.github.com>
2023-02-11 20:36:02 +0200

    create func edit_team

--- team/views.py
+++ team/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from team.models import Team
+
+
+def edit_team(request, pk):
+    team = get_object_or_404(Team, created_by=request.user, pk=pk)
+    return render(request, 'team/edit_team.html', {
+        'team': team
+    })
