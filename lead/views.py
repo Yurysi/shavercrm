81aeb2712dcb95bb47bc249d2d0ebccf5488805f
Yurysi <92383030+Yurysi@users.noreply.github.com>
2023-02-11 23:25:25 +0200

    add team in func

--- lead/views.py
+++ lead/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
@@ -6,21 +6,25 @@ from .forms import AddLeadForm
 from .models import Lead, Team
 from client.models import Client
 
+
 @login_required
 def leads_list(request):
-    leads = Lead.objects.filter(created_by = request.user, converted_to_client = False)
+    leads = Lead.objects.filter(created_by=request.user,
+                                converted_to_client=False)
     return render(request, 'lead/leads_list.html', {
         'leads': leads
     })
 
+
 @login_required
 def leads_detail(request, pk):
     lead = get_object_or_404(Lead, created_by=request.user, pk=pk)
-    lead = Lead.objects.filter(created_by = request.user).get(pk=pk)
+    lead = Lead.objects.filter(created_by=request.user).get(pk=pk)
     return render(request, 'lead/leads_detail.html',
                   {
                       'lead': lead,
                   })
+
 
 @login_required
 def leads_delete(request, pk):
@@ -28,6 +32,7 @@ def leads_delete(request, pk):
     lead.delete()
     messages.success(request, 'The lead was deleted.')
     return redirect('leads_list')
+
 
 @login_required
 def leads_edit(request, pk):
@@ -44,14 +49,15 @@ def leads_edit(request, pk):
         'form': form
     })
 
+
 @login_required
 def add_lead(request):
+    team = Team.objects.filter(created_by=request.user)[0]
     if request.method == 'POST':
         form = AddLeadForm(request.POST)
 
         if form.is_valid():
             team = Team.objects.filter(created_by=request.user)[0]
-
             lead = form.save(commit=False)
             lead.created_by = request.user
             lead.team = team
@@ -61,8 +67,10 @@ def add_lead(request):
     else:
         form = AddLeadForm()
     return render(request, 'lead/add_lead.html', {
-        'form': form
+        'form': form,
+        'team': team
     })
+
 
 @login_required
 def convert_to_client(request, pk):
@@ -70,11 +78,11 @@ def convert_to_client(request, pk):
     team = Team.objects.filter(created_by=request.user)[0]
 
     client = Client.objects.create(
-    name = lead.name,
-    email=lead.email,
-    description = lead.description,
-    created_by = request.user,
-    team= team,
+        name=lead.name,
+        email=lead.email,
+        description=lead.description,
+        created_by=request.user,
+        team=team,
     )
 
     lead.converted_to_client = True
